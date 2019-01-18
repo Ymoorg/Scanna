d8444378d452fccb258bbe66547c2ef3f9319e95
Ymoorg <44805108+Ymoorg@users.noreply.github.com>
2019-01-18 09:43:33 +0100

    Start

---
+++ index.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    console.log('Hello ' + name);
+}
+
+sayHello('Claes');
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
