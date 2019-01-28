ccb3062886fbbc9a10c6913eaaae95fc3748d2dc
Ymoorg <44805108+Ymoorg@users.noreply.github.com>
2019-01-28 20:02:05 +0100

    Test av databasfunktion

---
+++ js/Db_testdb.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "mysqluser",
+  password: "mysqlpassword",
+  database: "mydb"
+});
+
+function searchresult(theForm, con){
+    var searchresult = theFrom.search_input.value
+    var sql = 'SELECT `*` FROM `kontraktsnr` WHERE `idkontraktsnr` LIKE `"%"?"%" or `kundnamn` LIKE `"%"?"%"';
+    con.query(sql, [searchresult, searchresult], function (err, result) {
+    if (err) throw err;
+    document.getElementById(msg1).innerHTML=result;
+    });
+};
