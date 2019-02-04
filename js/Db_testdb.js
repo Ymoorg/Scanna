1b21d2222184f9d90d2dd08c89b9574ca85d0ca4
Ymoorg <44805108+Ymoorg@users.noreply.github.com>
2019-02-04 13:19:40 +0100

    alert

--- js/Db_testdb.js
+++ js/Db_testdb.js
@@ -1,12 +1,3 @@
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "mysqluser",
-  password: "mysqlpassword",
-  database: "mydb"
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+function processid(){
+  alert(1);
+}
