c6e58ca3922e7f4b53628f45c7ede845d121f591
Ymoorg <44805108+Ymoorg@users.noreply.github.com>
2019-02-04 13:30:10 +0100

    assetreader

--- js/Db_testdb.js
+++ js/Db_testdb.js
@@ -1,3 +1,8 @@
 function processid(){
   alert(document.getElementById('scanner_input').value);
+  //todo get info from api
+
+
+  document.getElementById('stoldskyddsnr').value = document.getElementById('scanner_input').value;
 }
+
